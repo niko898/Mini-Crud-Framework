b0eb2f85367cd2cf845dd1bc87a50fc1e7cb7bd2
wildniko88 <wildniko88@gmail.com>
2023-09-23 15:34:41 +0400

    database and view

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -2,14 +2,26 @@
 
 namespace Akimov\Crud\Controller;
 
+use Akimov\Crud\Database;
 use Akimov\Crud\Request;
 use Akimov\Crud\Response;
+use Akimov\Crud\View;
 
 class Index 
 {
     public function index(Request $request, Response $response): Response
     {
-        $response->setBody('Index');
+        $view = new View();
+        $pdo = Database::get();
+
+        $users = [
+            'name' => 'testname',
+            'lastname' => 'testlastname'
+        ];
+
+        $response->setBody($view->render('index.phtml', [
+            'users' => $users
+        ]));
         return $response;
     }
 }
