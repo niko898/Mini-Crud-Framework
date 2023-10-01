57332f6ad035456bfdc3f325a21db59b70571880
Mykola Akimov <mykola.akimov@bs2.lt>
2023-10-01 11:20:27 +0400

    finished migrate command

--- src/bin/commands/migrate.php
+++ src/bin/commands/migrate.php
@@ -1,26 +1,39 @@
 <?php
-/*
-$model_path = DIR_APPLICATION . '/src/Model/' . $model . '/Migration/' . $model . '.php';
-if(file_exists($model_path)){
-    require_once(DIR_APPLICATION . '/src/Core/Model/Model.php');
-    require_once($model_path);
-    var_dump(DIR_APPLICATION . '/src/Core/Model/Model.php');
-    $db = new Model();
-
-    $element = new $model();
-    var_dump($element->getTableName);
-}*/
 
 use Akimov\Crud\Core\Model\Model;
 $db = new Model();
-var_dump($db->get());
+$pdo = $db->get();
 
 $model_path = DIR_APPLICATION . '/src/Model/' . $model . '/Migration/' . $model . '.php';
 if(file_exists($model_path)){
     $model = 'Akimov\\Crud\\Model\\User\\Migration\\' . $model;
     require_once $model_path;
     $element = new $model();
-    var_dump($element->getTableName());
-    
+
+    $tableName = $element->getTableName();
+    $fields = $element->getTableFields();
+    $first_field_name = array_key_first($fields);
+
+    if(!isset($fields[$first_field_name]) || !isset($fields[$first_field_name]['key']) || !$fields[$first_field_name]['key'] ){
+        die("The fields are ampty");
+    }
+
+    // create table
+    $sql = "CREATE TABLE IF NOT EXISTS`" . DB_PREFIX . "$tableName` (
+        " . $first_field_name . " " . $fields[$first_field_name]['format'] . " 
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+    $pdo->query($sql);
+
+    foreach($fields as $field_name => $field_params){
+        $sql = "SHOW COLUMNS FROM `" . DB_PREFIX . "$tableName` LIKE '$field_name';";
+        
+		$query = $pdo->query($sql);
+
+		if (!$query->rowCount()) {
+			$sql = "ALTER TABLE `" . DB_PREFIX . "$tableName` ADD COLUMN `$field_name` " . $field_params['format'] . " ;";
+			$pdo->query($sql);
+		}
+    }
     
 }
